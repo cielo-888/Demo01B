17e1e6ecb4110acda667b9c8a9d052d275efb7be
Alumno <Alumno@LAB1507-14>
2024-08-20 19:51:39 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+       
+        public int Nombres { get; set; }
+        public int Apellidos { get; set; }
+
+        public int Edad { get; set; }
+        public string NombresCompletos { get; set; }
+
+        public string CalcularEdad()
+       
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+                mensaje = "Es mayor de edad";
+            return mensaje;
+
+        }
+
+        
+
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+            
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            Persona persona = new Persona();
+            persona.Nombres = "Cielo Jazmin";
+            persona.Apellidos = "Espinoza Cortez";
+
+            persona.AsignarNombresCompletos();
+            Console.WriteLine(persona.NombresCompletos);
+
+
+            Console.WriteLine("hola Mundo");
+            Console.Read();
+        }
+    }
+}
